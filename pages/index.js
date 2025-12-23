6dcaeedbba95926fe39d433691e0a8efe40e206f
jefersonbv <jefersonbv@gmail.com>
2025-12-23 17:16:30 +0000

    Adiciona a página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Marcelo, estou fazendo o curso do Felipe, esse é o primeiro desafio, logo, se você conseguiu ler esse texto manda um joia no WhatsApp</h1>
+}
+
+export default Home;
